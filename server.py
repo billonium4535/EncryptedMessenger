c0243a99d816eb10c0a0909ff3dbf404800c2346
billonium4535 <billy.sethgrimmett@gmail.com>
2025-08-15 09:51:52 +0100

    Created basic server

---
+++ server.py
@@ -0,0 +1,49 @@
+import socket
+import threading
+
+
+SERVER_IP = "0.0.0.0"
+SERVER_PORT = 5000
+
+clients = []
+
+
+def handle_client(connection, address):
+    print("Client {} connected".format(address))
+    while True:
+        try:
+            data = connection.recv(1024)
+            if not data:
+                break
+            broadcast(data, connection)
+        except:
+            break
+    print("Client {} disconnected".format(address))
+    clients.remove(connection)
+    connection.close()
+
+
+def broadcast(data, connection):
+    for client in clients:
+        if client != connection:
+            try:
+                client.send(data)
+            except:
+                client.close()
+                clients.remove(client)
+
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((SERVER_IP, SERVER_PORT))
+    server.listen(2)
+    print("Server listening on {}:{}".format(SERVER_IP, SERVER_PORT))
+
+    while True:
+        connection, address = server.accept()
+        clients.append(connection)
+        thread = threading.Thread(target=handle_client, args=(connection, address))
+        thread.start()
+
+
+main()
