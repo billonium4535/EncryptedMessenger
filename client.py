652a880aa8e3734eb3e05d43278820179ca4c6fb
billonium4535 <billy.sethgrimmett@gmail.com>
2025-08-19 16:11:10 +0100

    Removed old code

--- client.py
+++ client.py
@@ -89,35 +89,4 @@ def main():
     client.run()
 
 
-# def receive_message(sock, name):
-#     while True:
-#         try:
-#             message = sock.recv(1024).decode()
-#             if not message:
-#                 break
-#
-#             sys.stdout.write(f"\r{message}\n{name}> ")
-#             sys.stdout.flush()
-#         except:
-#             print("\n[!] Disconnected from server")
-#             sock.close()
-#             break
-
-
-# def main():
-#     name = input("Enter your name >")
-#     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     client.connect((SERVER_IP, SERVER_PORT))
-#
-#     threading.Thread(target=receive_message, args=(client, name,), daemon=True).start()
-#
-#     while True:
-#         message = input(f"{name}> ")
-#         if message.lower() == "/quit":
-#             client.close()
-#             break
-#         full_message = f"{name}> {message}"
-#         client.send(full_message.encode())
-
-
 main()
